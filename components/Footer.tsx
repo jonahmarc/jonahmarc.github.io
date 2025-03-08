51bb91fe49f66283c7bf56a8ceb15566728aa49e
jonahmarc <olympusfoxtrot@gmail.com>
2025-03-08 09:51:50 +0800

    add link to footer items

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import Image from 'next/image'
 
 export default function Footer() {
     return (
         <div className='flex items-center gap-4
                 h-[70px] py-10'>
-            <img className='w-auto h-[18px]' src="/icons/linkedin.png" alt="linkedin logo" />
-            <img className='w-auto h-[18px]' src="/icons/github.png" alt="github logo" />
-            <img className='w-auto h-[18px]' src="/icons/gmail.png" alt="gmail logo" />
+            <a href="https://www.linkedin.com/in/jonahmarc/" target="__blank"><img className='w-auto h-[18px]' src="/icons/linkedin.png" alt="linkedin logo" /></a>
+            <a href="https://github.com/jonahmarc" target="__blank"><img className='w-auto h-[18px]' src="/icons/github.png" alt="github logo" /></a>
+            <a href="mailto:jonahmarc.batomalaque@gmail.com" target="__blank"><img className='w-auto h-[18px]' src="/icons/gmail.png" alt="gmail logo" /></a>
         </div>
     )
 }
